856c1469b9ea9be76acc453211a483be825069aa
zahmis <meda829ratteru86@gmail.com>
2021-04-11 03:06:30 +0900

    fix _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
+import React from "react";
+// import { AppProps } from 'next/app';
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+
 function Minotaur({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Component {...pageProps} />
+  );
 }
 
 export default Minotaur;
