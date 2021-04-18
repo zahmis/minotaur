d1c814371533ed7729308269323643f30828cfa6
zahmis <meda829ratteru86@gmail.com>
2021-04-19 00:30:14 +0900

    tuning

---
+++ dist/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+export default function Home() {
+    // check ci
+    return (<div className={styles.container}>
+      <Head>
+        <title>Minotour</title>
+      </Head>
+      <main>
+        <div className="text-primary">Minotaur</div>
+      </main>
+    </div>);
+}
+//# sourceMappingURL=index.jsx.map
