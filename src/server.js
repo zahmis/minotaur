89686633c9ac022ff11a3db319d6aeb686132790
zahmis <meda829ratteru86@gmail.com>
2021-04-26 03:18:54 +0900

    fix

--- src/server.js
+++ src/server.js
@@ -2,25 +2,24 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 
+mongoose
+  .connect(process.env.MONGO_URI, { auto_reconnect: true, useNewUrlParser: true, useUnifiedTopology: true })
+  .catch((err) => {
+    console.log('error');
+  });
 
-mongoose.connect(process.env.MONGO_URI,
-   {auto_reconnect:true, useNewUrlParser: true, useUnifiedTopology: true}).catch((err)=>{
-    console.log('error')
-})
-
-console.log(process.env.MONGO_URI)
+console.log(process.env.MONGO_URI);
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-
 const kittySchema = new mongoose.Schema({
-  name: String
+  name: String,
 });
 
-const Kitten = mongoose.model('Kitten',kittySchema)
+const Kitten = mongoose.model('Kitten', kittySchema);
 
-const silence = new Kitten({name: "silence"})
+const silence = new Kitten({ name: 'silence' });
 silence.save().then(() => console.log('meow'));
 // const hoge = Kitten.find()
 return;
